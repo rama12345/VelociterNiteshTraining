1931f43bba3e7025c63af2694d45ac043d5fed40
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-14 11:47:45 +0530

    exercise2 added by nitesh

---
+++ Exercise2.java
@@ -0,0 +1,15 @@
+package com.velociter.nitesh.chapter2;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num = 1234.5678;
+//		int integer = (int)num;
+		long l = (long) num;
+		System.out.println(l);
+//		double db = num - (int)num;
+//		System.out.println(db);
+	}
+
+}
