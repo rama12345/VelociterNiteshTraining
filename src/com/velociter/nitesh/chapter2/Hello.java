b609772912db3968346d52741b952661cae262d7
Rama Shinde <rama@velociters.com>
2021-07-14 18:42:51 +0530

    Project added by rama

---
+++ src/com/velociter/nitesh/chapter2/Hello.java
@@ -0,0 +1,9 @@
+package com.velociter.nitesh.chapter2;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hi Nitesh, Welcome To Velociter");
+	}
+}
