d01c6e6a396afcc27cf81273fc937a4072b9e2e1
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-14 12:06:54 +0530

    directory related changes done by nitesh

---
+++ src/com/velociter/nitesh/chapter2/Exercise2.java
@@ -0,0 +1,11 @@
+package com.velociter.nitesh.chapter2;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num = 1234.5678;
+		long l = (long) num;
+		System.out.println(l);
+	}
+}
