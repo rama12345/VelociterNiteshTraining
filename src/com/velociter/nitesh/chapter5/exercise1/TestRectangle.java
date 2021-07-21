243fa23e991671c76862860d4c6112a455b16472
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-21 11:43:05 +0530

    changes of commit by nitesh

--- src/com/velociter/nitesh/chapter5/exercise1/TestRectangle.java
+++ src/com/velociter/nitesh/chapter5/exercise1/TestRectangle.java
@@ -2,23 +2,23 @@ package com.velociter.nitesh.chapter5.exercise1;
 
 public class TestRectangle {
 	public static void main(String args[]) {
-		Rectangle[] rects = new Rectangle[4];
+		Rectangle[] rectangles = new Rectangle[4];
 		Rectangle enclosing;
 
 		// Initialize the rectangles as arbitrary sizes and at arbitrary positions:
-		for (int i = 0; i < rects.length; ++i) {
-			rects[i] = new Rectangle(Math.random() * 100, Math.random() * 100, Math.random() * 100,
+		for (int i = 0; i < rectangles.length; ++i) {
+			rectangles[i] = new Rectangle(Math.random() * 100, Math.random() * 100, Math.random() * 100,
 					Math.random() * 100);
 			System.out.print("Coords of rectangle " + i + " are: ");
-			System.out.println(rects[i]);
+			System.out.println(rectangles[i]);
 		}
 
 		// Initialize the enclosing rectangle to be first rectangle
-		enclosing = rects[0];
+		enclosing = rectangles[0];
 
 		// Combine it with each the other rectangles in turn.
 		// This will result in the rectangle that encloses them all.
-		for (Rectangle rect : rects) {
+		for (Rectangle rect : rectangles) {
 			enclosing = enclosing.encloses(rect);
 		}
 
