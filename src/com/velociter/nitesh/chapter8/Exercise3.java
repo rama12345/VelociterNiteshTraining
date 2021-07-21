465873b1d3188b681c8ef26f2092d0df6ed1e3eb
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-21 11:46:12 +0530

    changes of commit by nitesh

--- src/com/velociter/nitesh/chapter8/Exercise3.java
+++ src/com/velociter/nitesh/chapter8/Exercise3.java
@@ -8,14 +8,14 @@ public class Exercise3 {
 		// TODO Auto-generated method stub
 		double minValue = -50.0, maxValue = +50.0;
 		// create the object of generating random number
-		Random theRandom = new Random();
-		double theRandomValue = 0.0;
+		Random random = new Random();
+		double randomValue = 0.0;
 		for (int i = 1; i <= 20; i++) {
 			// Checking for a valid range in between min-max without infinite ends.
 			if (Double.valueOf(maxValue - minValue).isInfinite() == false)
 				// generate random value of range min-max
-				theRandomValue = minValue + (maxValue - minValue) * theRandom.nextDouble();
-			System.out.printf(i + ")" + "%.2f\n", theRandomValue);
+				randomValue = minValue + (maxValue - minValue) * random.nextDouble();
+			System.out.printf(i + ")" + "%.2f\n", randomValue);
 		}
 	}
 }
