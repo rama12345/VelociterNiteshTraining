ef36b6f2d42b2985c52abf301ccddae4f8d1087e
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-21 18:52:38 +0530

    add exercise2 of chapter11 read data from file commited by nitesh

---
+++ src/com/velociter/nitesh/chapter11/Exercise1.java
@@ -0,0 +1,45 @@
+package com.velociter.nitesh.chapter11;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		// create the object of file to store
+		File txtFile = new File("D:/myFile.txt");
+		// create the object of FileInputStream to read data from file.
+		FileInputStream inputFile = null;
+		// try condition if that possible.
+		try {
+			inputFile = new FileInputStream(txtFile);
+		} catch (FileNotFoundException exception) {
+			exception.printStackTrace(System.err);
+			System.exit(1);
+		}
+		// fetch and reading data via input file channel.
+		FileChannel inputChannel = inputFile.getChannel();
+		// store data into the buffer at the time 48 byte.
+		ByteBuffer bufferStream = ByteBuffer.allocate(48);
+		try {
+			// check if data into the file not to empty.
+			while (inputChannel.read(bufferStream) != -1) {
+				// print all the character which it will store.
+				System.out.println("String read: " + ((ByteBuffer) (bufferStream.flip())).asCharBuffer().toString());
+				// Clear the buffer for the next read
+				bufferStream.clear(); 
+			}
+			System.out.println("EOF reached. ");
+			// Close the file and the channel
+			inputFile.close(); 
+		} catch (IOException exception) {
+			exception.printStackTrace(System.err);
+			System.exit(1);
+		}
+		System.exit(0);
+	}
+
+}
