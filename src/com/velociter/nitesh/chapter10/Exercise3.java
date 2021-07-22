62b12c1c0ce9241e85d0a062e190073247539327
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-22 18:31:52 +0530

    add the exercises of writing data to file by nitesh

---
+++ src/com/velociter/nitesh/chapter10/Exercise3.java
@@ -0,0 +1,78 @@
+package com.velociter.nitesh.chapter10;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exercise3 {
+	public static void main(String args[]) throws IOException {
+		// store the years into the string of array.
+		String[] monthNames = { "January", "February", "March", "April", "May", "June", "July", "August", "September",
+				"October", "November", "December" };
+		// set first with null.
+		String first = null;
+		// set monthString with null.
+		String monthOfString = null;
+		// taking the input form user day,month,year
+		int day = 0, month = 0, year = 0, j;
+		Scanner input = new Scanner(System.in);
+		// get the value of day, month, year from user.
+		System.out.println("Enter the day:");
+		day = input.nextInt();
+		System.out.println("Enter the Month:");
+		month = input.nextInt();
+		System.out.println("Enter the year:");
+		year = input.nextInt();
+		// check the condition of the day number pair. 
+		if (day == 1) {
+			first = "st";
+		}
+		if (day == 2) {
+			first = "nd";
+
+		}
+		if (day == 3) {
+			first = "rd";
+
+		}
+		if (day > 3) {
+			first = "th";
+		}
+		//convert the month number to month name 
+		for (int i = month - 1; i <= monthNames.length; i++) {
+			monthOfString = monthNames[i];
+			break;
+
+		}
+		// create the object of fileWriter to store data to file.
+		FileWriter filewriter = new FileWriter("Date1.txt");
+		// add the resulting day, month, year.
+		String firstFileDate = day + first + "," + monthOfString + "," + year;
+		System.out.println("first file date is:" + firstFileDate);
+		// put the data into first file
+		for (j = 0; j < firstFileDate.length(); j++) { // j variable for iteration
+			filewriter.write(firstFileDate.charAt(j));
+		}
+		filewriter.close();
+		System.out.println("Written in first file  sucessfully");
+		// creating a second file
+		FileWriter filewriterObject = new FileWriter("Date2.txt");
+		int convertedmonthStringIntoMonthNumber = 0;
+		for (int i = 0; i < monthNames.length; i++) // i for iteration
+		{
+			if (monthOfString == monthNames[i]) {
+				convertedmonthStringIntoMonthNumber = i + 1;
+			}
+		}
+		String secondFileDate = day + "/" + convertedmonthStringIntoMonthNumber + "/" + year;
+
+		// put the data into second file
+		for (int i = 0; i < secondFileDate.length(); i++) {
+			filewriterObject.write(secondFileDate.charAt(i));
+		}
+		System.out.println("Seond file date is:" + secondFileDate);
+		System.out.println("Written  in the second file succesfully");
+		filewriterObject.close();
+
+	}
+}
---
+++ src/com/velociter/nitesh/chapter10/Exercise4.java
@@ -0,0 +1,47 @@
+package com.velociter.nitesh.chapter10;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Exercise4 {
+	public static void main(String args[]) throws IOException {
+		String textOfString = "the fact cat ", word = "";
+		String arrayOfString[] = new String[14];
+		char character = ' ';
+		// initialize number of count to 0.
+		int i, numberOfWords = 0; 
+		// fetch the character with the length of string.
+		for (i = 0; i < textOfString.length(); i++) { 
+			// finding a one by one character from string using charAt()
+			character = textOfString.charAt(i);
+			// check the space or not
+			if (character != ' ') {
+				// adding a character in word
+				word = word + character;
+			} else {
+				// store number of words into the array of string.
+				arrayOfString[numberOfWords++] = word;
+				word = "";
+			}
+		}
+		int length = 2;
+		// fetch the end of length it.
+		for (int e = 0; e <= length - 1; e++) { 
+			// check the condition if number less it and apply sorting it.
+			if (e <= e + 1) {
+				String storeString = arrayOfString[e];
+				arrayOfString[e] = arrayOfString[e + 1];
+				arrayOfString[e + 1] = storeString;
+			}
+		}
+		// create an object of fileWriter to write data into file.
+		FileWriter fileWriter = new FileWriter("Arrayindex.txt");
+		for (i = 0; i <= length; i++) {
+			fileWriter.write(arrayOfString[i] + " "); // written the content into the file
+		}
+		fileWriter.close();
+		System.out.println("write successfully");
+
+	}
+
+}
