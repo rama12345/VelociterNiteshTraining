1e43f815ec83476e17c29be506a9e6a705463bb2
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-24 18:06:30 +0530

    Output the cards from the stack as four hands of 13 cards by nitesh

---
+++ src/com/velociter/nitesh/chapter14/exercise2/Exercise2.java
@@ -0,0 +1,48 @@
+package com.velociter.nitesh.chapter14.exercise2;
+
+import java.util.Vector;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.ListIterator;
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		String[] suits = { "C", "D", "H", "S" };
+		String[] cardValues = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+		int cardsInDeck = 52;
+		Vector<String> deck = new Vector<String>(cardsInDeck);
+		LinkedList<String> shuffledDeck = new LinkedList<String>();
+		Random chooser = new Random(); // Card chooser
+
+		// Load the deck
+		for (String suit : suits) {
+			for (String cardValue : cardValues) {
+				deck.add(cardValue + suit);
+			}
+		}
+
+		// Select cards at random from the deck to transfer to shuffled deck
+		int selection = 0; // Selected card index
+		for (int i = 0; i < cardsInDeck; ++i) {
+			selection = chooser.nextInt(deck.size());
+			shuffledDeck.add(deck.remove(selection));
+		}
+
+		// Deal the cards from the shuffled deck into four hands
+		StringBuffer[] hands = { new StringBuffer("Hand 1:"), new StringBuffer("Hand 2:"), new StringBuffer("Hand 3:"),
+				new StringBuffer("Hand 4:") };
+		ListIterator<String> cards = shuffledDeck.listIterator();
+
+		while (cards.hasNext()) {
+			for (StringBuffer hand : hands) {
+				hand.append(' ').append(cards.next());
+			}
+		}
+
+		// Display the hands
+		for (StringBuffer hand : hands) {
+			System.out.println(hand);
+		}
+	}
+}
