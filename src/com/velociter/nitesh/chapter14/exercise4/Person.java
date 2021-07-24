fc23b6257994fa67a8dd142ac8585d71e1a37834
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-24 18:07:56 +0530

    implements phone book details search by surname by nitesh

---
+++ src/com/velociter/nitesh/chapter14/exercise4/Person.java
@@ -0,0 +1,52 @@
+package com.velociter.nitesh.chapter14.exercise4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+
+public class Person implements Comparable<Person>, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String firstName; // First name of person
+	private String surname; // Second name of person
+	// constructor with parameter
+	public Person(String firstName, String surname) {
+		this.firstName = firstName;
+		this.surname = surname;
+	}
+
+	// Read a person from the keyboard
+	public static Person readPerson() {
+		String firstName = null;
+		String surname = null;
+		try {
+			// get the first and second name from user.
+			System.out.print("Enter first name: ");
+			firstName = keyboard.readLine().trim();
+			System.out.print("Enter surname: ");
+			surname = keyboard.readLine().trim();
+		} catch (IOException e) {
+			System.err.println("Error reading a name.");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return new Person(firstName, surname);
+	}
+
+	// Compare Person objects
+	public int compareTo(Person person) {
+		int result = surname.compareTo(person.surname);
+		return result == 0 ? firstName.compareTo(((Person) person).firstName) : result;
+	}
+
+	public String toString() {
+		return firstName + " " + surname;
+	}
+	
+	// store user input to buffer.
+	private static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+}
---
+++ src/com/velociter/nitesh/chapter14/exercise4/TryPhoneBook.java
@@ -0,0 +1,47 @@
+package com.velociter.nitesh.chapter14.exercise4;
+
+import java.util.Scanner;
+
+public class TryPhoneBook {
+	public static void main(String[] args) {
+		// create and object of phone book.
+		PhoneBook phoneBook = new PhoneBook(); // The phone book
+		Scanner input = new Scanner(System.in);
+		Person someone;
+		// iterate user entry with infinite loop.
+		for (;;) {
+			System.out.println("Enter 1 to enter a new phone book entry\n" + "Enter 2 to find the number for a name\n"
+					+ "Enter 9 to quit.");
+			int choice = 0; // Stores input selection
+			try {
+				choice = input.nextInt();
+			} catch (Exception e) {
+				System.out.println(e.getMessage() + "\nTry again.");
+				continue;
+			}
+			// switch case start pass the number to iterate.
+			switch (choice) {
+			case 1:
+				// add the user entry.
+				phoneBook.addEntry(BookEntry.readEntry());
+				break;
+			case 2:
+				// get the user entry.
+				someone = Person.readPerson();
+				BookEntry entry = phoneBook.getEntry(someone);
+				if (entry == null) {
+					System.out.println("The number for " + someone + " was not found. ");
+				} else {
+					System.out.println("The number for " + someone + " is " + entry.getNumber());
+				}
+				break;
+			case 9:
+				System.out.println("Ending program.");
+				return;
+			default:
+				System.out.println("Invalid selection, try again.");
+				break;
+			}
+		}
+	}
+}
