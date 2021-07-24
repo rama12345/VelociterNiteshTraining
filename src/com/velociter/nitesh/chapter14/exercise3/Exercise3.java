1139df73e296ca83a4a2e15e5da849d9c2669a80
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-24 18:07:08 +0530

    map to store names and telephone numbers by nitesh

---
+++ src/com/velociter/nitesh/chapter14/exercise3/Exercise3.java
@@ -0,0 +1,39 @@
+package com.velociter.nitesh.chapter14.exercise3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// creating HashMap object of type <String, String>
+		Map<Integer, String> hashMap = new HashMap<Integer, String>();
+		// get the key from user.
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the Number Between(10-20) : ");
+		int keyNumber = input.nextInt();
+		// adding key-value pairs to HashMap object
+		hashMap.put(10, "abhishek");
+		hashMap.put(11, "ajith");
+		hashMap.put(12, "alekhya");
+		hashMap.put(13, "amani");
+		hashMap.put(14, "aman");
+		hashMap.put(15, "shuhbam");
+		hashMap.put(16, "jhon");
+		hashMap.put(17, "jim");
+		hashMap.put(18, "jeky");
+		hashMap.put(19, "stev");
+		hashMap.put(20, "methu");
+		// check the condition it will be contain entered key.
+		if (hashMap.containsKey(keyNumber)) {
+			// store key to Object of value
+			Object value = hashMap.get(keyNumber);
+			System.out.println("Key : " + keyNumber + "\t" + "Value : " + value);
+		} else {
+			System.out.println("Not Founded Any Pair With Key!!!!");
+		}
+	}
+
+}
