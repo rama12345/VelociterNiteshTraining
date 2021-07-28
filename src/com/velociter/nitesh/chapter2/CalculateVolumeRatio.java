47d5c49a598b9147e4004496b559cac4576a0f16
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-28 16:01:56 +0530

    change class of CalculateVolumeRatio commited by nitesh

--- src/com/velociter/nitesh/chapter2/CalculateVolumeRatio.java
+++ src/com/velociter/nitesh/chapter2/CalculateVolumeRatio.java
@@ -1,23 +1,24 @@
 package com.velociter.nitesh.chapter2;
 
-public class Exercise4 {
+public class CalculateVolumeRatio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double sunRad = 865000.0 / 2.0; // Sun radius in miles is half the diameter
-		double earthRad = 7600.0 / 2.0; // Earth radius likewise
-		double fourOverThree = 4.0 / 3.0; // A convenient constant, 4/3
-		double sunVol = 0;
-		double earthVol = 0;
-		double ratioVol = 0;
+		double sunRadius = 865000.0 / 2.0; // Sun radius in miles is half the diameter
+		double earthRadius = 7600.0 / 2.0; // Earth radius likewise
+//		double fourOverThree = 4.0 / 3.0; // A convenient constant, 4/3
+		
+		double sunVolume = 0;
+		double earthVolume = 0;
+		double ratioVolume = 0;
 		// Find the volumes of earth and sun:
-		earthVol = fourOverThree * Math.PI * Math.pow(earthRad, 3);
-		sunVol = fourOverThree * Math.PI * Math.pow(sunRad, 3);
+		earthVolume = (4.0 / 3.0) * Math.PI * Math.pow(earthRadius, 3);
+		sunVolume = (4.0 / 3.0) * Math.PI * Math.pow(sunRadius, 3);
 		// Find the ratio of their volumes:
-		ratioVol = sunVol / earthVol;
+		ratioVolume = sunVolume / earthVolume;
 		// Output the results:
-		System.out.println("Volume of the earth is " + earthVol + " cubic miles");
-		System.out.println("Volume of the sun is " + sunVol + " cubic miles");
-		System.out.println("The sun's volume is " + ratioVol + " times greater than the earth's.");
+		System.out.println("Volume of the earth is " + earthVolume + " cubic miles");
+		System.out.println("Volume of the sun is " + sunVolume + " cubic miles");
+		System.out.println("The sun's volume is " + ratioVolume + " times greater than the earth's.");
 	}
 }
