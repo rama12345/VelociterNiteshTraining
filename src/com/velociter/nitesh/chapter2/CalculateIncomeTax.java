022de1dc88d26f3592684a280f5618451e54ec4e
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-28 17:25:21 +0530

    change of class CalculateIncomeTax commited by nitesh

--- src/com/velociter/nitesh/chapter2/CalculateIncomeTax.java
+++ src/com/velociter/nitesh/chapter2/CalculateIncomeTax.java
@@ -3,7 +3,6 @@ package com.velociter.nitesh.chapter2;
 public class CalculateIncomeTax {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		double income = 10000.00;
 	    double taxRate = 0.35; // 35%  corresponds to 0.35
 	    double taxAmount = income*taxRate;
