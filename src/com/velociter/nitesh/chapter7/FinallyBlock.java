8e54fe77e31c5460aa7582d522b8ed0074666dc5
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-28 18:31:05 +0530

    class of FinallyBlock commited by nitesh

--- src/com/velociter/nitesh/chapter7/FinallyBlock.java
+++ src/com/velociter/nitesh/chapter7/FinallyBlock.java
@@ -5,7 +5,7 @@ class ArithmeticExceptionExample {
 	int result = 0;
 	int iteratedCount = 0;
 
-	public void iteratedExceptionOccures() throws ArithmeticException {
+	public void iteratedExceptionOccures() throws Exception {
 		// get the string into array from the end of length
 		for (int iteration = 0;; iteration++) {
 			// generate the divisor random value of 100 multiple
@@ -17,14 +17,14 @@ class ArithmeticExceptionExample {
 	}
 }
 
-public class Exercise4 {
+public class FinallyBlock {
 	public static void main(String args[]) {
 		// creating the object of Arithmetic Exception Example
 		ArithmeticExceptionExample arithmeticExceptionExample = new ArithmeticExceptionExample();
 		try {
 			// find out the exception of Arithmetic Exception
 			arithmeticExceptionExample.iteratedExceptionOccures();
-		} catch (ArithmeticException e) {
+		} catch (Exception e) {
 			System.out.println("ArithmeticException Exception Occures:" + e.getMessage());
 		} finally {
 			System.out.println("Iteration count:" + arithmeticExceptionExample.iteratedCount);
