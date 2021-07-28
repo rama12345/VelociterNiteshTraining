da9d210eb28936fe3cef3999404f571d0204070c
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-28 17:25:55 +0530

    change of class CalculateVolumeRatio commited by nitesh

--- src/com/velociter/nitesh/chapter2/CalculateVolumeRatio.java
+++ src/com/velociter/nitesh/chapter2/CalculateVolumeRatio.java
@@ -3,7 +3,6 @@ package com.velociter.nitesh.chapter2;
 public class CalculateVolumeRatio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		double sunRadius = 865000.0 / 2.0; // Sun radius in miles is half the diameter
 		double earthRadius = 7600.0 / 2.0; // Earth radius likewise
 //		double fourOverThree = 4.0 / 3.0; // A convenient constant, 4/3
