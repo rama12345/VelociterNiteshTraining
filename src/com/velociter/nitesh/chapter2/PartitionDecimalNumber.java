fd7d190687d96df3db6e2bca9aa9c8610d513b9c
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-28 16:02:35 +0530

    change class of PartitionDecimalNumber commited by nitesh

--- src/com/velociter/nitesh/chapter2/PartitionDecimalNumber.java
+++ src/com/velociter/nitesh/chapter2/PartitionDecimalNumber.java
@@ -1,11 +1,12 @@
 package com.velociter.nitesh.chapter2;
 
-public class Exercise2 {
+public class PartitionDecimalNumber {
 	public static void main(String[] args) {
 		double doubleNumber = 1234.5678;
 		short shortNumber = (short) doubleNumber;
-		System.out.println(shortNumber);
+		System.out.println("Number is  : "+doubleNumber);
+		System.out.println("Intregral Part : "+shortNumber);
 		long longNumber = (long) (doubleNumber % 1.0 * 10000);
-		System.out.println(longNumber);
+		System.out.println("Decimal Part : "+longNumber);
 	}
 }
