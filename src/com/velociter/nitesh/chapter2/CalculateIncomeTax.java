c9e4cae0d85d5088196c079bbb5a1372321255c8
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-28 16:01:19 +0530

    change class of CalculateIncomeTax commited by nitesh

--- src/com/velociter/nitesh/chapter2/CalculateIncomeTax.java
+++ src/com/velociter/nitesh/chapter2/CalculateIncomeTax.java
@@ -1,15 +1,15 @@
 package com.velociter.nitesh.chapter2;
 
-public class Exercise3 {
+public class CalculateIncomeTax {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double income = 10000.00;
 	    double taxRate = 0.35; // 35%  corresponds to 0.35
-	    double tax = income*taxRate;
-	    int taxDollars = (int)tax;
+	    double taxAmount = income*taxRate;
+	    int taxDollars = (int)taxAmount;
 	    // find the randon number of taxCents
-	    int taxCents = (int)Math.round((tax - taxDollars));
+	    int taxCents = (int)Math.round((taxAmount - taxDollars));
 	    // Overall gross income in doller
 	    System.out.println("Gross income is $" + (int)income +"." + (int)Math.round((income - (int)income)));
 	    // final resulting tax pay in doller and taxCents in cents
