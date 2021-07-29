52bc27480aca54372b87e7612d22c2c02f6a6f06
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-29 18:04:31 +0530

    class of SentenceTokenizer changes commited by nitesh

--- src/com/velociter/nitesh/chapter8/SentenceTokenizer.java
+++ src/com/velociter/nitesh/chapter8/SentenceTokenizer.java
@@ -3,10 +3,9 @@ package com.velociter.nitesh.chapter8;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Exercise1 {
+public class SentenceTokenizer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String textOfString;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the Sentence : ");
