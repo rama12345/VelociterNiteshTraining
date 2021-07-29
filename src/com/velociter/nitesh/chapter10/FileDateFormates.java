e26f9e1dabf76e40e81461e3048b1f41adf483b7
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-07-29 17:48:00 +0530

    class of FileDateFormates changes commited by nitesh

--- src/com/velociter/nitesh/chapter10/FileDateFormates.java
+++ src/com/velociter/nitesh/chapter10/FileDateFormates.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Exercise2 {
+public class FileDateFormates {
 	public static void main(String args[]) throws IOException {
 		// store the years into the string of array.
 		String[] monthNames = { "January", "February", "March", "April", "May", "June", "July", "August", "September",
