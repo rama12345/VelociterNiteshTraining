3c53ea5473d79d82bd9ef8cf94953cdbe91d4936
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-08-02 19:10:48 +0530

    changes of code in FileSeperator commited by nitesh

--- src/com/velociter/nitesh/chapter10/FileSeperations.java
+++ src/com/velociter/nitesh/chapter10/FileSeperations.java
@@ -2,30 +2,43 @@ package com.velociter.nitesh.chapter10;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileSeperations {
-	public static void main(String args[]) {
-		try {
-			// Accept a string
-			String textOfstring = "Hand/ling /n Java /" + " FileWriter / FileReader";
-			char characterOfString;
+	public static void main(String args[]) throws IOException {
+		// Accept a string
+		// String textOfstring = "Hand/ling /n Java /" + " FileWriter / FileReader";
+		String textOfString;
 
-			// attach a file to FileWriter
-			FileWriter fileWriter = new FileWriter("D:/myFile.txt");
+		// take input from user.
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a Sentence with Delimitter : ");
 
-			// read character wise from string and write into FileWriter
-			for (int i = 0; i < textOfstring.length(); i++) {
-				characterOfString = textOfstring.charAt(i);
-				if (characterOfString == '/') {
-					// do nothing
-				} else {
-					fileWriter.write(textOfstring.charAt(i));
+		// check the validation of input.
+		if (input.hasNext("[A-Za-z/]*")) {
+			textOfString = input.nextLine();
+
+			// Split method matches the given delimiter and split the string based on it.
+			String[] output = textOfString.split("/");
+
+			String filePath = "D:/myFile.txt";
+			try {
+				FileWriter fileWriter = new FileWriter(filePath);
+
+				// apply for each loop for iteration.
+				for (String splitString : output) {
+					System.out.println(splitString);
+
+					// write word of string to file.
+					fileWriter.write(splitString + "\n");
 				}
+				System.out.println("Writing successful....");
+				fileWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			System.out.println("Writing successful");
-			fileWriter.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+		} else {
+			System.out.println("Please Enter Valid Sentence.");
 		}
 	}
 }
