20f89d003fa12a9e15793af8db28c8fd5ccd95cb
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-08-06 16:31:30 +0530

    add some changes of reinter date formatte commited by nitesh

--- src/com/velociter/nitesh/chapter10/DateFormateFiles.java
+++ src/com/velociter/nitesh/chapter10/DateFormateFiles.java
@@ -6,83 +6,123 @@ import java.util.Scanner;
 
 public class DateFormateFiles {
 	public static void main(String args[]) throws IOException {
+		try {
 
-		// store the years into the string of array.
+			takeInputFromUser();
+
+		} catch (Exception e) {
+			System.out.println("you Enterd wrong input..");
+			takeInputFromUser();
+		}
+	}
+
+	public static void takeInputFromUser() throws IOException {
+
+		int day = 0, month = 0, year = 0, yearLength = 0;
+		System.out.println("Enter the day in Number:");
+		Scanner scanner = new Scanner(System.in);
+
+		try {
+			// take input from user
+			day = scanner.nextInt();
+
+			System.out.println("Enter the Month in Number:");
+			month = scanner.nextInt();
+
+			System.out.println("Enter the  year in Number:");
+			year = scanner.nextInt();
+
+			// finding year of length
+			yearLength = Integer.toString(year).length();
+
+			// checking user input
+			if (day > 31 || month > 12 || yearLength > 4 || yearLength < 4) {
+
+				System.out.println("you Enterd wrong input..");
+				while (true) {
+					System.out.println("press 1 for continue");
+					System.out.println("press 2 for Exit");
+					int myChoice = scanner.nextInt();
+
+					switch (myChoice) {
+					case 1:
+						takeInputFromUser();
+						break;
+					case 2:
+						System.out.println("Thank You For Using!!!");
+						System.exit(0);
+						break;
+
+					}
+
+				}
+
+			} else {
+				dateFormatFun(day, month, year);
+			}
+		} catch (Exception e) {
+
+			System.out.println("You Enter Wrong input..");
+			takeInputFromUser();
+		}
+	}
+
+	public static void dateFormatFun(int day, int month, int year) throws IOException {
 		String[] monthNames = { "January", "February", "March", "April", "May", "June", "July", "August", "September",
 				"October", "November", "December" };
 
-		// set first with null.
-		String first = null;
-
-		// set monthString with null.
-		String monthOfString = null;
-
-		// taking the input form user day,month,year
-		int day = 0, month = 0, year = 0, j;
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the Day, Month and Year :");
-		if (input.hasNext("[0-9]")) {
-			// get the value of day, month, year from user.
-			day = input.nextInt();
-			month = input.nextInt();
-			year = input.nextInt();
-
-			// check the condition of the day number pair.
-			if (day == 1) {
-				first = "st";
-			}
-			if (day == 2) {
-				first = "nd";
-
-			}
-			if (day == 3) {
-				first = "rd";
-
-			}
-			if (day > 3) {
-				first = "th";
-			}
-
-			// convert the month number to month name
-			for (int i = month - 1; i <= monthNames.length; i++) {
-				monthOfString = monthNames[i];
-				break;
-			}
-
-			// create the object of fileWriter to store data to file.
-			FileWriter filewriter = new FileWriter("D:/Date1.txt",true);
-
-			// add the resulting day, month, year.
-			String firstFileDate = day + first + " " + monthOfString + " " + year;
-			System.out.println("First File Date is : " + firstFileDate);
-
-			// put the data into first file
-			for (j = 0; j < firstFileDate.length(); j++) { // j variable for iteration
-				filewriter.write(firstFileDate.charAt(j));
-			}
-			filewriter.close();
-			System.out.println("Written in first file  sucessfully.....");
-
-			// creating a second file
-			FileWriter filewriterObject = new FileWriter("D:/Date2.txt");
-			int convertedmonthStringIntoMonthNumber = 0;
-			for (int i = 0; i < monthNames.length; i++) // i for iteration
-			{
-				if (monthOfString == monthNames[i]) {
-					convertedmonthStringIntoMonthNumber = i + 1;
-				}
-			}
-			String secondFileDate = day + "/" + convertedmonthStringIntoMonthNumber + "/" + year;
-
-			// put the data into second file
-			for (int i = 0; i < secondFileDate.length(); i++) {
-				filewriterObject.write(secondFileDate.charAt(i));
-			}
-			System.out.println("Second File Date is : " + secondFileDate);
-			System.out.println("Written In The Second File Succesfully......");
-			filewriterObject.close();
-		} else {
-			System.out.println("Please Enter a Valid DateFormate.");
+		String first = null, monthString = null;
+//checking the day number 
+		if (day == 1) {
+			first = "st";
 		}
+		if (day == 2) {
+			first = "nd";
+
+		}
+		if (day == 3) {
+			first = "rd";
+
+		}
+		if (day > 3) {
+			first = "th";
+		}
+		// convert the month number to month name
+		for (int i = month - 1; i <= monthNames.length; i++) {
+			monthString = monthNames[i];
+			break;
+
+		}
+		// creating a file
+		FileWriter filewriter = new FileWriter("D:/Date1.txt");
+
+		String firstFileDate = day + first + " " + monthString + " " + year;
+		System.out.println("first file date is:" + firstFileDate);
+
+		// put the data into first file
+		for (int j = 0; j < firstFileDate.length(); j++) { // j variable for iteration
+			filewriter.write(firstFileDate.charAt(j));
+		}
+		filewriter.close();
+		System.out.println("Written in first file  sucessfully");
+		// creating a second file
+		FileWriter filewriterObject = new FileWriter("D:/Date2.txt", true);
+		int convertedmonthStringIntoMonthNumber = 0;
+		for (int i = 0; i < monthNames.length; i++) // i for iteration
+		{
+			if (monthString == monthNames[i]) {
+				convertedmonthStringIntoMonthNumber = i + 1;
+			}
+		}
+		String secondFileDate = day + "/" + convertedmonthStringIntoMonthNumber + "/" + year;
+
+		// put the data into second file
+		for (int i = 0; i < secondFileDate.length(); i++) {
+			filewriterObject.write(secondFileDate.charAt(i));
+		}
+		System.out.println("Seond file date is:" + secondFileDate);
+		System.out.println("Written  in the second file succesfully");
+		filewriterObject.close();
+
 	}
 }
