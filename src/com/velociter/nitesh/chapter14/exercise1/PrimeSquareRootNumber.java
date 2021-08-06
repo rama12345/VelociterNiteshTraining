565c11a282a29af4caafbf617d0ecf6ebb2953b7
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-08-06 17:00:30 +0530

    add changes of prime square root number comited by nitesh

--- src/com/velociter/nitesh/chapter14/exercise1/PrimeSquareRootNumber.java
+++ src/com/velociter/nitesh/chapter14/exercise1/PrimeSquareRootNumber.java
@@ -5,48 +5,22 @@ import java.util.Scanner;
 public class PrimeSquareRootNumber {
 
 	public static void main(String[] args) {
-//		int status = 1;
-//		int num = 3;
-//		Scanner input = new Scanner(System.in);
-//		System.out.println("Enter the number : ");
-//		if(input.hasNext("[0-9]*")) {
-//			int n = input.nextInt();
-//			long startTime = System.currentTimeMillis();
-//			System.out.println("Before Execution in Second : " + startTime);
-//			for (int i = 2; i <= n;) {
-//				for (int j = 2; j <= Math.sqrt(num); j++) {
-//					if (num % j == 0) {
-//						status = 0;
-//						break;
-//					}
-//				}
-//				if (status != 0) {
-//					System.out.print(num+" ");
-//					i++;
-//				}
-//				status = 1;
-//				num++;
-//			}
-//			long entTime = System.currentTimeMillis();
-//			System.out.println("\nAfter Execution in Second : " + entTime);
-//			System.out.println("Difference Between Execution Second : " + (entTime - startTime) / 1000F+"seconds");
-//		}else {
-//			System.out.println("Please Enter Valid Input.");
-//		}
-
-		int N; 
+		int number; 
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the value of n:");
-		N = scanner.nextInt();
+		System.out.println("Enter the Value of Numbers :");
+		number = scanner.nextInt();
 		long startTime = System.currentTimeMillis();
 		System.out.println("Before Execution Time is : " + startTime);
-		if (N >= 1) {
-			System.out.println(" prime numbers are:");
+		if (number >= 1) {
+			System.out.println("Prime Numbers Are : ");
 		}
-
-		for (int j = 2; j <= N; j++) {
+		
+		// iteration of loop with N
+		for (int j = 2; j <= number; j++) {
 			int flag = 0;
+			// check if its not prime
 			for (int i = 2; i <= j; i++) {
+				// check the condition of complete divisible 
 				if (j % i == 0) {
 					flag = flag + 1;
 				}
