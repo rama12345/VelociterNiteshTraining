591690bdd64cf6b85f6f963f994064cdd7f69606
niteshpidiparas <niteshpidiparas.velociters@gmail.com>
2021-08-06 17:40:41 +0530

    add changes of code commited by nitesh

--- src/com/velociter/nitesh/chapter14/exercise4/Person.java
+++ src/com/velociter/nitesh/chapter14/exercise4/Person.java
@@ -1,53 +1,49 @@
 package com.velociter.nitesh.chapter14.exercise4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Serializable;
+public class Person {
+	
+	// data member of person.
+	static int id = TryPhoneBook.people.size();
+	private String name;
+	private String surname;
+	private String phoneNumber;
+	private String email;
+	private String address;
 
-public class Person implements Comparable<Person>, Serializable {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private String firstName; // First name of person
-	private String surname; // Second name of person
-
-	// constructor with parameter
-	public Person(String firstName, String surname) {
-		this.firstName = firstName;
+	// constructor with arguments.
+	Person(String name, String surname, String phoneNumber, String email, String address) {
+		this.name = name;
 		this.surname = surname;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.address = address;
+		id++;
 	}
 
-	// Read a person from the keyboard
-	public static Person readPerson() {
-		String firstName = null;
-		String surname = null;
-		try {
-			// get the first and second name from user.
-			System.out.print("Enter first name: ");
-			firstName = keyboard.readLine().trim();
-			System.out.print("Enter surname: ");
-			surname = keyboard.readLine().trim();
-		} catch (IOException e) {
-			System.err.println("Error reading a name.");
-			e.printStackTrace();
-			System.exit(1);
-		}
-		return new Person(firstName, surname);
+	// create getters. 
+	String getName() {
+		return name;
 	}
 
-	// Compare Person objects
-	public int compareTo(Person person) {
-		int result = surname.compareTo(person.surname);
-		return result == 0 ? firstName.compareTo(((Person) person).firstName) : result;
+	String getSurname() {
+		return surname;
 	}
 
+	String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	String getEmail() {
+		return email;
+	}
+
+	String getAddress() {
+		return address;
+	}
+
+	@Override
 	public String toString() {
-		return firstName + " " + surname;
+		return "\n\nName: " + getName() + "\nSurname: " + getSurname() + "\nPhone number: " + getPhoneNumber()
+				+ "\nEmail: " + getEmail() + "\nAddress: " + getAddress();
 	}
-
-	// store user input to buffer.
-	private static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 }
--- src/com/velociter/nitesh/chapter14/exercise4/TryPhoneBook.java
+++ src/com/velociter/nitesh/chapter14/exercise4/TryPhoneBook.java
@@ -1,50 +1,159 @@
 package com.velociter.nitesh.chapter14.exercise4;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TryPhoneBook {
-	public static void main(String[] args) {
-		// create and object of phone book.
-		PhoneBook phoneBook = new PhoneBook(); // The phone book
-		Scanner input = new Scanner(System.in);
-		Person someone;
-		
-		// iterate user entry with infinite loop.
-		for (;;) {
-			System.out.println("Enter 1 to enter a new phone book entry\n" + "Enter 2 to find the number for a name\n"
-					+ "Enter 9 to quit.");
-			int choice = 0; // Stores input selection
-			try {
-				choice = input.nextInt();
-			} catch (Exception e) {
-				System.out.println(e.getMessage() + "\nTry again.");
-				continue;
-			}
-		
-			// switch case start pass the number to iterate.
+	static String filePath = "D:/Addresses.txt";
+	private static Scanner in = new Scanner(System.in);
+	
+	// create an object of file to store all details of person to file.
+	private static File file = new File(filePath);
+	
+	// create list of person to extract list of person details.
+	static List<Person> people = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+		readPeopleFromFile();
+		showMainMenu();
+	}
+
+	// method-1 for find person details with your choice.
+	private static void findPerson() throws IOException {
+		System.out.println("1. Find with name");
+		System.out.println("2. Find with surname");
+
+		String choice;
+		do {
+			choice = in.nextLine();
 			switch (choice) {
-				case 1:
-					// add the user entry.
-					phoneBook.addEntry(BookEntry.readEntry());
-					System.out.println("Your Entry Has Been Done.......");
-					break;
-				case 2:
-					// get the user entry.
-					someone = Person.readPerson();
-					BookEntry entry = phoneBook.getEntry(someone);
-					if (entry == null) {
-						System.out.println("The number for " + someone + " was not found. ");
-					} else {
-						System.out.println("The number for " + someone + " is " + entry.getNumber());
-					}
-					break;
-				case 9:
-					System.out.println("Ending program.");
-					return;
-				default:
-					System.out.println("Invalid selection, try again.");
-					break;
+			case "1":
+				findByName();
+				break;
+			case "2":
+				findBySurname();
+				break;
+			default:
+				System.out.print("Choose 1 or 2: ");
+			}
+		} while (!choice.equals("1") && !choice.equals("2"));
+		System.out.println();
+		showMainMenu();
+	}
+
+	// method-2 for find person details with surname.
+	private static void findBySurname() {
+		System.out.print("Enter surname: ");
+		String surnameToFind = in.nextLine();
+		int matches = 0;
+		for (Person person : people) {
+			if (person.getSurname().equals(surnameToFind)) {
+				System.out.println(person);
+				matches++;
 			}
 		}
+		if (matches <= 0) {
+			System.out.println("There is no person with this surname");
+		}
+	}
+
+	// method-3 for find person details with name.
+	private static void findByName() {
+		System.out.print("Enter name: ");
+		String nameToFind = in.nextLine();
+		int matches = 0;
+		for (Person person : people) {
+			if (person.getName().equals(nameToFind)) {
+				System.out.println(person);
+				matches++;
+			}
+		}
+		if (matches <= 0) {
+			System.out.println("There is no person with this name ");
+		}
+	}
+
+	// method-4 for add person details.
+	private static void addPerson() throws IOException {
+
+		System.out.println("Enter Name: ");
+		String name = in.nextLine();
+		System.out.println("Enter Surname: ");
+		String surname = in.nextLine();
+		System.out.println("Enter Phone Number: ");
+		String phoneNumber = in.nextLine();
+		System.out.println("Enter Email: ");
+		String email = in.nextLine();
+		System.out.println("Enter Address: ");
+		String address = in.nextLine();
+
+		Person person = new Person(name, surname, phoneNumber, email, address);
+		addToFile(person);
+		people.add(person);
+		System.out.println("Added person number: " + person);
+		System.out.println();
+		showMainMenu();
+	}
+
+	// method-4 for add person details to file.
+	private static void addToFile(Person person) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+			writer.write(person.getName() + "\r\n" + person.getSurname() + "\r\n" + person.getPhoneNumber() + "\r\n"
+					+ person.getEmail() + "\r\n" + person.getAddress() + "\r\n\r\n");
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	// method-5 for read person details from file.
+	private static boolean readPeopleFromFile() throws IOException {
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			String name = null;
+			while ((name = reader.readLine()) != null) {
+				Person person = new Person(name, reader.readLine(), reader.readLine(), reader.readLine(),
+						reader.readLine());
+				people.add(person); // adds person to the list
+				reader.readLine();
+			}
+			return true;
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		return false;
+	}
+
+	// method-4 for main menu for user interface to there choice.
+	private static void showMainMenu() throws IOException {
+		System.out.println("----------PHONE BOOK DIRECTORY----------");
+		System.out.println("1. Add person");
+		System.out.println("2. Find person");
+		System.out.println("3. Show all contacts");
+		System.out.println("4. Close program");
+
+		String choice;
+		do {
+			choice = in.nextLine();
+			switch (choice) {
+			case "1":
+				addPerson();
+				break;
+			case "2":
+				findPerson();
+				break;
+			case "3":
+				System.out.println(people);
+				System.out.println();
+				showMainMenu();
+				break;
+			case "4":
+				System.out.println("Thank Your For Using!!!!");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Enter number from 1 to 4");
+			}
+		} while (!choice.equals("4"));
 	}
 }
